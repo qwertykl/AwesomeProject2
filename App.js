cc149e84f2380bbd8022a9b4d7a11822bd647cf3
qwertykl <35958823+qwertykl@users.noreply.github.com>
2018-10-19 13:16:22 -0700

    Update App.js

--- App.js
+++ App.js
@@ -50,4 +50,11 @@ export default class App extends React.Component {
   
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
